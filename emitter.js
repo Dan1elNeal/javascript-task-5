0c51021a81704dfb32ed550d076ef52ac72b8df9
Dan1elNeal <ishindanil96@gmail.com>
2017-11-01 23:10:12 +0500

    Удалил лишнее

--- emitter.js
+++ emitter.js
@@ -70,30 +70,6 @@ function getEmitter() {
             });
 
             return this;
-        },
-
-        /**
-         * Подписаться на событие с ограничением по количеству полученных уведомлений
-         * @star
-         * @param {String} event
-         * @param {Object} context
-         * @param {Function} handler
-         * @param {Number} times – сколько раз получить уведомление
-         */
-        several: function (event, context, handler, times) {
-            console.info(event, context, handler, times);
-        },
-
-        /**
-         * Подписаться на событие с ограничением по частоте получения уведомлений
-         * @star
-         * @param {String} event
-         * @param {Object} context
-         * @param {Function} handler
-         * @param {Number} frequency – как часто уведомлять
-         */
-        through: function (event, context, handler, frequency) {
-            console.info(event, context, handler, frequency);
         }
     };
 }
